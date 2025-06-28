5c42cf26ecd057ad8501dd939c8ab38866df59e3
asifcaspi <asifcaspi1@gmail.com>
2025-06-28 14:44:01 +0300

    a

--- server/app/routers/upload.py
+++ server/app/routers/upload.py
@@ -37,6 +37,7 @@ if not os.path.exists(output_file):
         os.system(f"wget -O \"{output_file}\" {url}")
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
 model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
 model = model.to(device).eval()
 
